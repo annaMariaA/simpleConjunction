d5e707b2e0b32b04d4f08b0b2d16510317c2030b
Alasdair Clarke <alasdair.clarke@gmail.com>
2021-08-26 12:08:47 +0100

    code edits

--- cluster/run_model.R
+++ cluster/run_model.R
@@ -5,45 +5,27 @@ library(patchwork)
 
 options(mc.cores = 4)
 
-d <- read_csv("accuracy_rt_data.txt") %>%
-  mutate(
-    targ_present = as_factor(targPresent),
-    targ_present = fct_recode(targ_present, present = "1", absent = "0"),
-    trial_type = as_factor(targetType),
-    difficulty = as.numeric(as_factor(difficulty))) %>%
-  select(observer, trial="trial_type", targ="targ_present", difficulty, n, rt, accuracy) 
-
-d$response <- NA
-d$response[which(d$targ=="present" & d$accuracy==1)] = 1
-d$response[which(d$targ=="present" & d$accuracy==0)] = 0
-d$response[which(d$targ=="absent" & d$accuracy==1)] = 0
-d$response[which(d$targ=="absent" & d$accuracy==0)] = 1
-
-
-
-
-# remove some outliers
-d <- filter(d, rt > 0.2, rt < 20)
-
+d <- read_csv("data_for_model.csv", 
+              col_types = "fffffdfi")
 
 formula <- bf(rt | dec(response) ~ 
-              0 + targ:trial  + targ:trial:difficulty  + (0 + targ:trial + targ:trial:difficulty|p|observer), 
+                # the first part is the linear predictor for drift rate
+                0 + targ:trial  + targ:trial:difficulty  + 
+                (0 + targ:trial + targ:trial:difficulty|p|observer), 
+              # bs: boundary separation 
               bs ~ 0 + trial + (0 + trial|p|observer), 
-              ndt ~ 0 + trial,
+              # ndt: non-decision time
+              ndt ~ 0 + trial + (1|p|observer),
+              # bias: starting bias for TA or TP
               bias ~ 0 + trial + (0 + trial|p|observer))
 
-get_prior(formula,
-          data = d, 
-          family = wiener(link_bs = "identity", 
-                          link_ndt = "identity", 
-                          link_bias = "identity"))
 
 prior <- c(
-  prior("normal(0, 2)", class = "b"),
+  set_prior("normal(0, 3)", class = "b"),
   set_prior("normal(1.5, 1)", class = "b", dpar = "bs"),
-  set_prior("normal(0.2, 0.1)", class = "b", dpar = "ndt"),
-  set_prior("normal(0.5, 0.2)", class = "b", dpar = "bias")
-)
+  set_prior("normal(0.2, 0.05)", class = "b", dpar = "ndt"),
+  set_prior("normal(0.5, 0.2)", class = "b", dpar = "bias"),
+  set_prior("normal(0, 0.05)", class = "sd", dpar = "ndt"))
 
 
 make_stancode(formula, 
@@ -64,7 +46,7 @@ initfun <- function() {
   list(
     b = rnorm(tmp_dat$K),
     b_bs = runif(tmp_dat$K_bs, 1, 2),
-    b_ndt = runif(tmp_dat$K_ndt, 0.01, 0.1),
+    b_ndt = runif(tmp_dat$K_ndt, 0.01, 0.2),
     b_bias = rnorm(tmp_dat$K_bias, 0.5, 0.1),
     sd_1 = runif(tmp_dat$M_1, 0.5, 1),
     z_1 = matrix(rnorm(tmp_dat$M_1*tmp_dat$N_1, 0, 0.01),
@@ -72,7 +54,6 @@ initfun <- function() {
     L_1 = diag(tmp_dat$M_1)
   )
 }
-
 
 
 fit_wiener <- brm(formula,
